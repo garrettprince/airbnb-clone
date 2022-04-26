970532ae349d6a46a2dce94ec5209c1cf3d1af7a
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-04-25 17:41:14 -0700

    Changed button text to resolve Vercel deployment error

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,7 +11,7 @@ function Banner() {
             />
             <div className="absolute top-1/2 w-full text-center">
                 <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-                <button className="text-purple-500 bg-white px-6 py-3 sm:px-10 sm:py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
+                <button className="text-purple-500 bg-white px-6 py-3 sm:px-10 sm:py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I am flexible</button>
             </div>
         </div>
     )
